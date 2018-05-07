8034ee5b6208e87c19aa075371f18dc2768b3abb
cailoo <caiosilvaazeredo@hotmail.com>
2018-05-07 00:01:55 -0300

    Site versão 2.0
    Reformulação total do site

---
+++ assets/mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// checando campos vazios
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "preenchimento inválido";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+
+$to = caiosilvaazeredo@gmail.com; 
+$email_subject = "Website Contact Form:  $name";
+$email_body = "Você recebeu um novo contato através da pagina oficial.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
